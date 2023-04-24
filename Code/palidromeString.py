1ee3bf7978c06c7daeca101217d08bd27a387413
Swaraj Endait <endait.swaraj@gmail.com>
2023-04-24 17:54:38 +0530

    Update palindromeString.py
    Added alternative for number palindrome!

--- Code/palidromeString.py
+++ Code/palidromeString.py
@@ -1,4 +1,3 @@
-'''
 # To check if string is a palindrome
 strInput = input("Enter string to see if it is palindrome or not: ")
 strInput = strInput.casefold()
@@ -7,12 +6,12 @@ if strInput == rev:
     print(strInput, "is a palindrome! Palindrome Version:", rev)
 else:
     print(strInput, "is not a palindrome!")
-'''
 
+#Alternative for num palindrome
 num = int(input("Enter no.: "))
 strify = str(num)
 rev = ''.join(reversed(strify))
-if num == rev:
+if strify == rev:
     print(num, "is palindrome! Palindrome: ", rev)
 else:
     print(num, "is not palindrome!")
